b7f645e7661a715031f486b8f2704af02a7cb80f
gabriellorandi <Progweb1301>
2021-07-24 16:42:48 -0300

    adicionando icone em tab

--- src/util/config.js
+++ src/util/config.js
@@ -1,8 +1,8 @@
 
 class Config {
 
-  DRINK_COLLECTION_URL = "http://localhost:3001/drink";
-
-}
-
-export default Config;
+    DRINK_COLLECTION_URL = "http://localhost:3001/drink";
+  
+  }
+  
+  export default Config;
